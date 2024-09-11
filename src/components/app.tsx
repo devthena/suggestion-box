5e3b3005befba07e328ccabb4edb2c131bc29dcb
Athena <athena@parthenon.app>
2024-09-11 00:25:45 -0700

    feat(app): Added Material UI components and updated how suggestions are displayed

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,19 @@
 import React, { useEffect } from 'react';
 
+import {
+  Card,
+  CardContent,
+  Container,
+  Grid2,
+  Divider,
+  Avatar,
+} from '@mui/material';
+
+import { Comment } from '@mui/icons-material';
+
 import { useDataState } from '../hooks';
 import { Suggestion } from '../lib/types';
+import { formatDate } from '../lib/utils';
 
 export const App: React.FC = () => {
   const { suggestions, generateMockData } = useDataState();
@@ -14,18 +26,28 @@ export const App: React.FC = () => {
   if (!suggestions.size) return <main>Loading...</main>;
 
   return (
-    <main>
+    <Container>
       <h1>Suggestion Box</h1>
-      <div>
-        {[...suggestions.values()].map((suggestion: Suggestion) => (
-          <div key={suggestion.id}>
-            <h2>{suggestion.title}</h2>
-            <p>Author: {suggestion.author.name}</p>
-            <p>{suggestion.description}</p>
-            <p>Comment Count: {suggestion.comments.length}</p>
-          </div>
-        ))}
-      </div>
-    </main>
+      <Grid2 container spacing={2}>
+        <Grid2 size={8}>
+          {[...suggestions.values()].map((suggestion: Suggestion) => (
+            <Card key={suggestion.id}>
+              <CardContent>
+                <h2>{suggestion.title}</h2>
+                <div>
+                  <Avatar>{suggestion.author.id}</Avatar>
+                  <p>{suggestion.author.name}</p>
+                </div>
+                <p>
+                  {suggestion.comments.length} <Comment />
+                </p>
+                <Divider />
+                Posted on {formatDate(suggestion.created_at)}
+              </CardContent>
+            </Card>
+          ))}
+        </Grid2>
+      </Grid2>
+    </Container>
   );
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,18 @@ import Chance from 'chance';
 
 export const chance = new Chance();
 
+export const formatDate = (date: Date) => {
+  const updatedMonth = ('0' + (date.getMonth() + 1)).slice(-2);
+  const updatedDay = ('0' + date.getDate()).slice(-2);
+  const updatedDate = `${updatedMonth}-${updatedDay}-${date.getFullYear()}`;
+
+  const updatedHours = ('0' + date.getHours()).slice(-2);
+  const updatedMinutes = ('0' + date.getMinutes()).slice(-2);
+  const updatedTime = `${updatedHours}:${updatedMinutes}`;
+
+  return `${updatedDate} at ${updatedTime}`;
+};
+
 export const generateId = () => {
   return chance.guid({ version: 4 });
 };
