3b5f92b40b010ea54b3c28996a5c7906a5b39a91
Athena <athena@parthenon.app>
2024-09-13 02:37:53 -0700

    fix(components): Fix for file rename issue on Vercel

---
+++ src/components/App.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { Container, Stack } from '@mui/material';
+
+import { useDataState } from '../hooks';
+
+import { Header } from './Header';
+import { SuggestionList } from './SuggestionList';
+import { SuggestionThread } from './SuggestionThread';
+
+import styles from '../styles/app.module.scss';
+
+/**
+ * The main entry point of the application that manages the layout of
+ * the header, suggestion list, and suggestion thread
+ * @component
+ */
+export const App: React.FC = () => {
+  const { suggestions, generateMockData } = useDataState();
+  const [isDataGenerated, setIsDataGenerated] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (suggestions.size > 0 || isDataGenerated) return;
+
+    generateMockData();
+    setIsDataGenerated(true);
+  }, [isDataGenerated, suggestions.size, generateMockData]);
+
+  if (!suggestions.size) return <Container>Loading...</Container>;
+
+  return (
+    <Container className={styles.main}>
+      <Header />
+      <Stack className={styles.body} direction="row">
+        <SuggestionList />
+        <SuggestionThread />
+      </Stack>
+    </Container>
+  );
+};
---
+++ src/components/Header.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { AddCircleOutline, KeyboardBackspace } from '@mui/icons-material';
+import { Button, Container, Stack } from '@mui/material';
+
+import { useDataState } from '../hooks';
+import { SuggestionCreate } from './SuggestionCreate';
+
+import styles from '../styles/header.module.scss';
+
+/**
+ * Provides the interface for adding new suggestions and generating mock data
+ * such as suggestions and comments. It also contains navigation for going back
+ * to the suggestion list from a selected suggestion.
+ * @component
+ */
+export const Header: React.FC = () => {
+  const {
+    selected,
+    addNewComment,
+    addNewSuggestion,
+    generateComment,
+    generateSuggestion,
+    selectSuggestion,
+    toggleModal,
+  } = useDataState();
+
+  const handleCommentGenerate = () => {
+    if (!selected) return;
+
+    const newComment = generateComment(selected.id);
+    addNewComment(newComment);
+  };
+
+  const handleSuggestionGenerate = () => {
+    const newSuggestion = generateSuggestion();
+    addNewSuggestion(newSuggestion);
+  };
+
+  return (
+    <Container className={styles.header}>
+      <h1>Suggestion Box</h1>
+      <Stack direction={{ xs: 'column', sm: 'row' }} spacing={1}>
+        <Stack
+          className={selected ? `${styles.hasSelected}` : ''}
+          direction={{ xs: 'column', sm: 'row' }}
+          spacing={2}>
+          <Button
+            className={styles.button}
+            endIcon={<AddCircleOutline />}
+            onClick={toggleModal}
+            variant="contained"
+            size="medium">
+            ADD SUGGESTION
+          </Button>
+          <Button
+            className={styles.button_light}
+            onClick={handleSuggestionGenerate}
+            size="small">
+            Generate Suggestion
+          </Button>
+        </Stack>
+        {selected && (
+          <Stack
+            direction="row"
+            justifyContent={selected ? 'space-between' : 'center'}>
+            <Button
+              className={`${styles.back} ${styles.button_light}`}
+              onClick={() => selectSuggestion(null)}
+              startIcon={<KeyboardBackspace />}
+              size="small">
+              Back
+            </Button>
+            <Button
+              className={styles.button_light}
+              onClick={handleCommentGenerate}
+              size="small">
+              Generate Comment
+            </Button>
+          </Stack>
+        )}
+      </Stack>
+      <SuggestionCreate />
+    </Container>
+  );
+};
